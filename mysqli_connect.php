daf039f83dc836e9042bec19618f71aff0857b3f
emeldo001 <emehchiemerie9@gmail.com>
2025-10-18 00:19:05 +0100

    Changes in the SQL connection file

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -1,22 +1,9 @@
-
-
 <?php
-//mysql_select_db('SchoolPortal',  mysql_connect('localhost','root',''))or die(mysql_error());
-// $servername = "localhost";
-// $username = "u269972180_fundsgrowths";
-// $password = "Julietandrews120@gmail.com";
-// $dbname = "u269972180_fundsgrowths";
-
 
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "erisdb";
-
-// $servername = "localhost";
-// $username = "portygrc_portico";
-// $password = "mZMURW7kUHny";
-// $dbname = "portygrc_portico";
 
 // Create connection
 $con = mysqli_connect($servername, $username, $password,$dbname);
@@ -26,7 +13,4 @@ mysqli_set_charset($con, 'utf8');
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error($con));
 }
-
-
 ?>
-
